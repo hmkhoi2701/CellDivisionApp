1e976e574a657f2981901f0e2d42f56b4ba9d993
hmkhoi2701 <hominhkhoi2701@gmail.com>
2022-07-05 14:57:39 +0700

    add Images

---
+++ sourcecode/src/core/cell/Cell.java
@@ -0,0 +1,13 @@
+package core.cell;
+
+import java.util.ArrayList;
+
+import core.component.Component;
+
+public abstract class Cell {
+	private String name;
+	private float length;
+	private ArrayList<Component> components;
+	
+	public
+}
