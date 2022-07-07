a197ae85071355db5f87db2b0dc3fd9afa797c06
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2022-07-07 12:46:05 +0700

    Delete OOP.DSAI.20212.13-feature-menu directory

--- OOP.DSAI.20212.13-feature-menu/sourcecode/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module CellApp {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	opens application.controller to javafx.graphics, javafx.fxml;
-	opens application.screentests to javafx.graphics, javafx.xml;
-}
