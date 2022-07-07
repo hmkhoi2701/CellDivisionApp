1e15c713d452de8a6703c9a0b837dc794abe8328
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2022-07-07 12:40:25 +0700

    Add files via upload

---
+++ OOP.DSAI.20212.13-feature-menu/sourcecode/src/core/cell/Cell.java
@@ -0,0 +1,11 @@
+package core.cell;
+
+import java.util.ArrayList;
+
+import core.component.Component;
+
+public abstract class Cell {
+	private String name;
+	private float length;
+	private ArrayList<Component> components;
+}
