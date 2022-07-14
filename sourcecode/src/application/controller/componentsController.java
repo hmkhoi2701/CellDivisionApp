a2b334310473945daea9fc3c6fd0fa1cb6528a39
hmkhoi2701 <hominhkhoi2701@gmail.com>
2022-07-15 00:27:25 +0700

    draft_update

---
+++ sourcecode/src/application/controller/componentsController.java
@@ -0,0 +1,32 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class componentsController {
+
+    @FXML
+    private ComboBox<?> ListOfCell;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label functionLabel;
+
+    @FXML
+    private ImageView myImageView;
+
+    @FXML
+    private Button Ribosomes;
+
+    @FXML
+    void SelectCell(ActionEvent event) {
+
+    }
+
+}
--- sourcecode/src/core/cell/Cell.java
+++ sourcecode/src/core/cell/Cell.java
@@ -3,9 +3,38 @@ package core.cell;
 import java.util.ArrayList;
 
 import core.component.Component;
+import core.division.GrowMethod;
+import core.division.ReproduceMethod;
 
 public abstract class Cell {
 	private String name;
-	private float length;
+	private String size;
 	private ArrayList<Component> components;
+	protected GrowMethod growMethod;
+	protected ReproduceMethod reproduceMethod;
+	protected String imgDir;
+	
+	public Cell(String name, String size) {
+		super();
+		this.name = name;
+		this.size = size;
+		this.imgDir = "/guiImages/CellImages/";
+	}
+	
+	public void addComponent(ArrayList<Component> toAdd) {
+		for (Component component: toAdd) {
+			components.add(component);
+		}
+	}
+
+	public GrowMethod getGrowMethod() {
+		return growMethod;
+	}
+
+	public ReproduceMethod getReproduceMethod() {
+		return reproduceMethod;
+	}
+	
+	
+	
 }
