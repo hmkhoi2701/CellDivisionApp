df8422db5e6105c5eb61b1a70e83fd5b872cdaad
hmkhoi2701 <hominhkhoi2701@gmail.com>
2022-07-15 22:03:34 +0700

    update Menu

--- sourcecode/src/application/controller/chooseCellController.java
+++ sourcecode/src/application/controller/chooseCellController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import application.Main;
 import core.cell.Cell;
 import core.cell.EukaryoticCell;
+import core.cell.ProkaryoticCell;
 import core.component.Component;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,29 +25,67 @@ public class chooseCellController {
 	private ArrayList<Component>components;
     @FXML
     private VBox buttonBox;
-
+    
     @FXML
-    void backBtnPressed(ActionEvent event) throws IOException {
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/chooseCellTypeMenu.fxml"));
-		chooseCellTypeController controller = new chooseCellTypeController(cells,components,"Comp");
-		loader.setController(controller);
-		Parent root = loader.load();
-        Scene scene = new Scene(root);
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/guiImages/5470363.png")));
-        stage.setScene(scene);
-        stage.setTitle("Cell Application");
-        stage.show();
-    }
-
-    public chooseCellController(ArrayList<Cell>cells, ArrayList<Component>components, String prevChoice) {
+    public void initialize() {
     	if (prevChoice.equals("Eu")){
+    		buttonBox.setSpacing(30);
     		for (Cell cell: cells) {
     			if (cell instanceof EukaryoticCell) {
     				Button cellName = new Button(cell.getName().toUpperCase());
-    				this.buttonBox.getChildren().add(cellName);
+    				cellName.setStyle("-fx-font-family: Lucida Bright; -fx-font-size: 18pt;"
+    						+ "-fx-font-weight: Bold; -fx-background-color:  #FFDAB9; -fx-background-radius: 20");
+    				cellName.setPrefSize(350, 53);
+    				cellName.setMaxHeight(cellName.USE_PREF_SIZE);
+    				cellName.setMaxWidth(cellName.USE_PREF_SIZE);
+    				buttonBox.getChildren().add(cellName);
     			}
     		}
     	}
+    	if (prevChoice.equals("Pro")) {
+    		buttonBox.setSpacing(70);
+    		for (Cell cell: cells) {
+    			if (cell instanceof ProkaryoticCell) {
+    				Button cellName = new Button(cell.getName().toUpperCase());
+    				cellName.setStyle("-fx-font-family: Lucida Bright; -fx-font-size: 18pt;"
+    						+ "-fx-font-weight: Bold; -fx-background-color:  #FFDAB9; -fx-background-radius: 20");
+    				cellName.setPrefSize(350, 53);
+    				cellName.setMaxHeight(cellName.USE_PREF_SIZE);
+    				cellName.setMaxWidth(cellName.USE_PREF_SIZE);
+    				buttonBox.getChildren().add(cellName);
+    			}
+    		}
+    	}
+    	Button backBtn = new Button("BACK");
+    	backBtn.setStyle("-fx-font-family: Lucida Bright; -fx-font-size: 18pt;"
+				+ "-fx-font-weight: Bold; -fx-background-color:  #FFDAB9; -fx-background-radius: 20");
+    	backBtn.setPrefSize(350, 53);
+    	backBtn.setMaxHeight(backBtn.USE_PREF_SIZE);
+    	backBtn.setMaxWidth(backBtn.USE_PREF_SIZE);
+    	backBtn.setOnAction(event -> {
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/chooseCellTypeMenu.fxml"));
+    		chooseCellTypeController controller = new chooseCellTypeController(cells,components,"Comp");
+    		loader.setController(controller);
+    		Parent root = null;
+			try {
+				root = loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            Scene scene = new Scene(root);
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.getIcons().add(new Image(Main.class.getResourceAsStream("/guiImages/5470363.png")));
+            stage.setScene(scene);
+            stage.setTitle("Cell Application");
+            stage.show();
+    	});
+    	buttonBox.getChildren().add(backBtn);
+    }
+    
+    public chooseCellController(ArrayList<Cell>cells, ArrayList<Component>components, String prevChoice) {
+    	this.cells = cells;
+    	this.components = components;
+    	this.prevChoice = prevChoice;
     }
 }
--- sourcecode/src/application/controller/chooseCellTypeController.java
+++ sourcecode/src/application/controller/chooseCellTypeController.java
@@ -35,21 +35,34 @@ public class chooseCellTypeController{
 
     @FXML
     void eukaryotesBtnClicked(ActionEvent event) throws IOException {
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/menu.fxml"));
-    	chooseCellController controller = new chooseCellController(cells,components,"Eu");
-    	loader.setController(controller);
-		Parent root = loader.load();
-        Scene scene = new Scene(root);
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/guiImages/5470363.png")));
-        stage.setScene(scene);
-        stage.setTitle("Cell Application");
-        stage.show();
+    	if (prevChoice.equals("Comp")) {
+	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/chooseCellMenu.fxml"));
+	    	chooseCellController controller = new chooseCellController(cells,components,"Eu");
+	    	loader.setController(controller);
+			Parent root = loader.load();
+	        Scene scene = new Scene(root);
+	        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/guiImages/5470363.png")));
+	        stage.setScene(scene);
+	        stage.setTitle("Cell Application");
+	        stage.show();
+    	}
     }
 
     @FXML
-    void prokaryotesBtnClicked(ActionEvent event) {
-
+    void prokaryotesBtnClicked(ActionEvent event) throws IOException {
+    	if (prevChoice.equals("Comp")) {
+	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/chooseCellMenu.fxml"));
+	    	chooseCellController controller = new chooseCellController(cells,components,"Pro");
+	    	loader.setController(controller);
+			Parent root = loader.load();
+	        Scene scene = new Scene(root);
+	        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/guiImages/5470363.png")));
+	        stage.setScene(scene);
+	        stage.setTitle("Cell Application");
+	        stage.show();
+    	}
     }
 
     public chooseCellTypeController(ArrayList<Cell>cells, ArrayList<Component>components,String prevChoice) {
